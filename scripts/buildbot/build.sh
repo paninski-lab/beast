74cb0fbf86ed3c97f3555b0212f7d564398fe349
themattinthehatt <themattinthehatt@gmail.com>
2025-06-01 15:47:12 -0400

    continuous integration with self-hosted runner

---
+++ scripts/buildbot/build.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+# Setup a SIGINT handler. Not sure why, but this is necessary for SIGINT (Ctrl-C) to cancel this script.
+handle_sigint() {
+    echo "Caught SIGINT (Ctrl+C). Exiting..."
+    exit 130  # Exit with a specific code (128 + signal number)
+}
+# Trap the SIGINT signal and call the handle_sigint function
+trap handle_sigint SIGINT
+
+set -e
+
+USER=paninski-lab
+REPO_NAME=beast
+
+BASE_DIR=/local/$(whoami)/builds
+TARGET_DIR=$BASE_DIR/$(date '+%Y_%m_%d-%H_%M_%S')
+CONDA_ENV=beast_build
+
+# Check if the PR number argument is provided
+if [ $# -eq 0 ]; then
+  echo "Error: Pull request number is required as the first argument."
+  echo "Usage: $0 <PR_NUMBER>"
+  exit 1
+fi
+PR_NUMBER="$1"
+
+echo "Running from $(hostname)"
+
+ml Miniforge-24.7.1-2
+conda activate $CONDA_ENV
+
+# Remove builds older than 24 hours
+find "$BASE_DIR" -maxdepth 1 -type d -mtime +0 -print0 | while IFS= read -r -d $'\0' directory; do
+  # Skip the starting directory itself.
+  if [[ "$directory" != "$BASE_DIR" ]]; then
+      echo "Removing directory: $directory"
+      rm -rf "$directory"
+  fi
+done
+
+# Get the code of the PR.
+# For efficiency, rather than cloning, it inits a blank repo
+# and fetches just the code we need.
+git init "$TARGET_DIR"
+cd "$TARGET_DIR"
+git remote add upstream "https://github.com/$USER/$REPO_NAME.git"
+git fetch upstream "refs/pull/$PR_NUMBER/merge"
+git checkout FETCH_HEAD
+
+# Run with html reporting.
+pytest --html=report.html --self-contained-html
---
+++ scripts/buildbot/build_srun.sh
@@ -0,0 +1,5 @@
+# About the flags:
+#   -X is needed for github cancel to work.
+#     Otherwise srun needs two successive ctrl-C / SIGINT to cancel
+#   -u was found to enable colors. --pty would also work.
+srun -X -u -t 1:00:00 -c4 --mem=8g --gres=gpu:2 beast/buildbot/build.sh $@
